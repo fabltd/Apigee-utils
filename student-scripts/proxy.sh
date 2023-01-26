8ccc36e64dffaee3c2d20c7ddbd2a5dc68c25f5b
Jeremy <jeremy@fabcontrols.co.uk>
2023-01-26 13:37:27 +0000

    Moved proxy script

--- student-scripts/proxy.sh
+++ student-scripts/proxy.sh
@@ -5,4 +5,4 @@ gcloud config set compute/zone us-east1-b
 
 #Setup PortForward
 echo -e "Setup SSH Tunnel"
-gcloud compute ssh vs-code-server  -- -NL 8080:localhost:8080
+gcloud compute ssh vs-code-server  -- -NL 8080:localhost:8080 -4
