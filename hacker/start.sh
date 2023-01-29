7f079945f3dee23b19c6e2666d1443b8e98d381b
Jeremy <jeremy@fabcontrols.co.uk>
2023-01-29 19:02:05 +0000

    Fixes/startup for hackpage

---
+++ hacker/start.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#Start Hacker Page
+
+#Generate GCP Access Token
+export TOKEN=$(gcloud auth print-access-token)
+
+#Set Enviroment Vars
+export ORG=$GOOGLE_CLOUD_PROJECT
+#Apigee Enviroment
+export ENV=eval
+
+# Get Apigee Host from Apigee
+HOST=$(curl -s "https://apigee.googleapis.com/v1/organizations/$ORG/envgroups/eval-group" \
+    -X GET \
+    -H "Authorization: Bearer $TOKEN" \
+    | jq ".hostnames[1]" -r
+    )
+  
+echo "Updating Apigee Host: $HOST" 
+sed -i 's~http://localhost:8080/v1/~https://'$HOST'/show-me-now/v0/~g' index.html
+
+#Start server
+npm install
+npm start
