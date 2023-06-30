05dc849535c98c78c3e82abf62dfda2a0dd560b2
Jeremy <fabltd@users.noreply.github.com>
2023-06-30 16:54:08 +0100

    Fixes lab5 script

--- hacker/start.sh
+++ hacker/start.sh
@@ -7,7 +7,7 @@ export TOKEN=$(gcloud auth print-access-token)
 #Set Enviroment Vars
 export ORG=$GOOGLE_CLOUD_PROJECT
 #Apigee Enviroment
-export ENV=eval
+export ENV=test-env
 
 # Get Apigee Host from Apigee
 HOST=$(curl -s "https://apigee.googleapis.com/v1/organizations/$ORG/envgroups/test-env-group" \
@@ -17,7 +17,7 @@ HOST=$(curl -s "https://apigee.googleapis.com/v1/organizations/$ORG/envgroups/te
     )
   
 echo "Updating Apigee Host: $HOST" 
-sed -i 's~http://localhost:8080/v1/~https://'$HOST'/show-me-now/v0/~g' index.html
+sed -i 's~https://null/show-me-now/v0/~https://'$HOST'/show-me-now/v0/~g' index.html
 
 #Start server
 npm install --silent
